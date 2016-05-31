ff49ba67094fe22903e3c3dd7cc3a2ea0867091e
lovebube <lovebube@hotmail.com>
2016-05-31 17:27:32 +0900

    get file from url

--- src/main/java/kr/ac/uos/ai/JSONGetter.java
+++ src/main/java/kr/ac/uos/ai/JSONGetter.java
@@ -10,7 +10,6 @@ import java.net.URL;
  * @author Chan Yeon, Cho
  * @version 0.0.1 - SnapShot
  *          on 2016-05-31 enemy
- * @link http://ai.uos.ac.kr:9000/lovebube/UIMA_Management_Client
  */
 
 public class JSONGetter {
--- src/main/java/kr/ac/uos/ai/JSONParser.java
+++ src/main/java/kr/ac/uos/ai/JSONParser.java
@@ -4,7 +4,6 @@ package kr.ac.uos.ai;
  * @author Chan Yeon, Cho
  * @version 0.0.1 - SnapShot
  *          on 2016-05-31 enemy
- * @link http://ai.uos.ac.kr:9000/lovebube/UIMA_Management_Client
  */
 
 public class JSONParser {
